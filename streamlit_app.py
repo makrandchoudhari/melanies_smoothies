82fc36d97b1f0ab21c10c4b1f525eefe3d5a513f
makrandchoudhari <makrandchoudhari@gmail.com>
2025-07-28 13:04:13 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredients: "
     ,my_dataframe
